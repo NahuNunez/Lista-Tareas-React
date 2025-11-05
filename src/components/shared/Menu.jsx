4f1a324402146d5a060cbaa0d8348fb517c6c726
Nahuel Nuñez <nunezn53@gmail.com>
2025-11-05 03:15:02 -0300

    instalo react hook form

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -4,12 +4,14 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const Menu = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">TO DO LIST con React</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      </Container>
-    </Navbar>
+    <div>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">TO DO LIST con React</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </Container>
+        </Navbar>
+    </div>
   );
 }
 
